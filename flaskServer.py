581ad3ccbd9e745e9c9ba62deb4f713b465339a7
Harsh <harsha@inspirho.in>
2017-12-02 15:01:28 +0530

    minimal webapp shell is now done for the sending page. todo- captcha and ip filtering

--- flaskServer.py
+++ flaskServer.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from __future__ import print_function
+import sys
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,4 +9,13 @@ app = Flask(__name__)
 def hello():
 	return render_template("form.html")
 
-app.run(debug= True)
+@app.route("/sendmail", methods = ['POST','GET'])
+def sendmail():
+	if request.method == 'POST':
+		return 'sending mail "'+request.form['body']+'" from "'+request.form['from']+'" to "'+request.form['to']+'"...'
+	elif request.method == 'GET':
+		return 'you were not supposed to land here! send a mail from the home page'
+
+
+if __name__ == '__main__':
+	app.run(debug= True)
