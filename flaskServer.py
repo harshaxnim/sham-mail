ff7ca67ad38564b8422deea854ad5bbdba5f9d93
Harsh <harsha@inspirho.in>
2017-12-19 16:51:37 +0530

    New Template with flex-boxes

--- flaskServer.py
+++ flaskServer.py
@@ -12,10 +12,10 @@ def hello():
 @app.route("/sendmail", methods = ['POST','GET'])
 def sendmail():
 	if request.method == 'POST':
-		return 'sending mail "'+request.form['body']+'" from "'+request.form['from']+'" to "'+request.form['to']+'"...'
+		return 'sending mail "'+request.form['body']+'" from "'+request.form['from']+'" to "'+request.form['to']+'"...<br>you are <b>'+request.remote_addr+'</b>'
 	elif request.method == 'GET':
 		return 'you were not supposed to land here! send a mail from the home page'
 
 
 if __name__ == '__main__':
-	app.run(debug= True)
+	app.run("0.0.0.0",debug= True)
